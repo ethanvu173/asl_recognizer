4cf0c881d5ef2b422e354ffed1bea1b04cea017f
ethanvu173 <ethanvu173@gmail.com>
2026-05-08 21:29:01 -0400

    Finished files to collect data, train an ASL-detecting model, and recognize the alphabet

---
+++ recognize.py
@@ -0,0 +1,44 @@
+import cv2
+import joblib
+import numpy as np
+import mediapipe as mp
+from utils import extract_landmarks
+from utils import init_hand_model
+from utils import draw_landmarks_on_image
+
+
+MODEL_PATH = "./hand_landmarker.task"
+
+# Set up video capture and hand landmarking model
+classifier = joblib.load('asl_alpha_classifier.pkl')
+capture = cv2.VideoCapture(0)
+detector = init_hand_model(MODEL_PATH)
+
+success, frame = capture.read()
+while success:
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame_mp = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame_rgb)
+    output = detector.detect(frame_mp)
+
+    if output.hand_landmarks:
+        for landmark in output.hand_landmarks:
+            frame = draw_landmarks_on_image(frame, output)
+
+        # Get and make predictions on hand landmarks
+        coords = np.array(extract_landmarks(output)).reshape(1, -1)
+        prediction = classifier.predict(coords)[0]
+        proba = classifier.predict_proba(coords).max()
+
+        # Requires >0.5 confidence to determine letter
+        if proba > 0.5:
+            cv2.putText(frame, f"{prediction}  {proba:.0%}",
+                        (10, 50),cv2.FONT_HERSHEY_SIMPLEX,
+                        2, (0, 0, 255), 2)
+
+    cv2.imshow("ASL Alphabet Recognizer", frame)
+    success, frame = capture.read()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+
+# Creates a hand landmarking model using the model at the location path
+def init_hand_model(path):
+    base_options = python.BaseOptions(model_asset_path=path)
+    options = vision.HandLandmarkerOptions(base_options=base_options,
+                                           num_hands=1)
+    detector = vision.HandLandmarker.create_from_options(options)
+
+    return detector
+
+# Normalizes the hand landmarks with respect to the wrist landmark
+def extract_landmarks(detection_results):
+    wrist = detection_results.hand_landmarks[0]
+    coords = []
+
+    for landmark in wrist:
+        coords.extend([landmark.x-wrist[0].x,
+                       landmark.y-wrist[0].y,
+                       landmark.z-wrist[0].z])
+
+    return coords
+
+# From the Google MediaPipe example for hand landmarks
+# https://colab.research.google.com/github/googlesamples/mediapipe/blob/main/examples/hand_landmarker/python/hand_landmarker.ipynb
+def draw_landmarks_on_image(rgb_image, detection_result):
+  hand_landmarks_list = detection_result.hand_landmarks
+  handedness_list = detection_result.handedness
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+    handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+    vision.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks,
+      vision.HandLandmarksConnections.HAND_CONNECTIONS,
+      vision.drawing_styles.get_default_hand_landmarks_style(),
+      vision.drawing_styles.get_default_hand_connections_style())
+
+    # Get the top left corner of the detected hand's bounding box.
+    height, width, _ = annotated_image.shape
+    x_coordinates = [landmark.x for landmark in hand_landmarks]
+    y_coordinates = [landmark.y for landmark in hand_landmarks]
+    text_x = int(min(x_coordinates) * width)
+    text_y = int(min(y_coordinates) * height) - 10
+
+    # Draw handedness (left or right hand) on the image.
+    cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                1, (0,255,0), 1, cv2.LINE_AA)
+
+  return annotated_image
